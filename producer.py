2551c2f23e8f117481b625e2e652f9ef1d0d98e3
Han Lee <bonafidehan@gmail.com>
2017-12-14 23:58:06 -0800

    Initial conversion of the Server to an HTTP server that listens to consumer registrations.

--- producer.py
+++ producer.py
@@ -1,8 +1,25 @@
 import argparse
+import re
 import SocketServer
 
 class Server(object):
-    """Toy UDP server that echoes."""
+    """Toy streaming server. Produces a stream UDP packets to registered consumers.
+
+    To register a consumer, send a PUT request to the producer. The HTTP request's client IP
+    address is then registered. The payload must contain a number which the port that the
+    consumer will be listening on. The DELETE verb deletes the registartion.
+
+    In the commandline example below, nc is used to register a consumer listening at port
+    6714 and then deregisters itself.
+
+    $ nc SERVER PORT
+    PUT / HTTP/1.1
+    6714
+
+    $ nc SERVER PORT
+    DELETE / HTTP/1.1
+    6714
+    """
 
     @staticmethod
     def parse_args():
@@ -12,17 +29,32 @@ class Server(object):
                             help='Set the port to listen for UDP packets')
         return parser.parse_args()
 
-    class UDPHandler(SocketServer.BaseRequestHandler):
-        """A toy handler that echoes."""
-        def handle(self):
-            received = self.request[0].strip()
-            socket = self.request[1]
-            socket.sendto('Ack: {}'.format(received), self.client_address)
+    class RegistrationHandler(BaseHTTPRequestHandler):
+        """A handler for registering new consumers.
+
+        New consumers of the streaming server should PUT. To deregister, existing consumers
+        should DELETE. The only vaild resource path is /. The payload must contain a port number
+        of the consumer.
+
+        The class has static state. Python's standard HTTP server framework does not allow
+        access to a handler's instance state.
+
+        Thread-safe. Its static state is protected by a lock.
+        """
+        lock = threading.Lock()
+        # Guarded by lock.
+        registered = set()
+
+        def do_PUT(self):
+            pass
+
+        def do_DELETE(self):
+            pass
 
     def __init__(self, port):
-        """Constructs a UDP Server listening at the provided port."""
-        self.udp_server = SocketServer.UDPServer(('localhost', port), Server.UDPHandler)
-        self.udp_server.serve_forever()
+        """Constructs an HTTP Server listening at the provided port."""
+        address = ('localhost', port)
+        httpd = BaseHTTPServer.HTTPServer(address, Server.RegistrationHandler)
 
 if __name__ == '__main__':
     args = Server.parse_args()
